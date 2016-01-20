389b3949b771d44af73229aa1cf6cadf97810d6c
prem <mr.premprakash@gmail.com>
2016-01-20 21:57:24 +0530

    Product section

--- frontend/js/controller/produts.js
+++ frontend/js/controller/produts.js
@@ -1,4 +1,4 @@
-myDemoApp.controller('productListCtrl', function($scope, $http, productService) {
+cablinkApp.controller('productListCtrl', function($scope, $http, productService) {
     $scope.products = [];
     productService.getProducts().then(function(data) {
         $scope.products = data;
